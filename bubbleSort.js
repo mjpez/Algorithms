570d0a48c86240b8c711da2f86c2b69461a31096
Mark Lopez <molopez492@gmail.com>
2018-04-16 15:46:18 -0400

    feat: add bubbleSort

---
+++ bubbleSort.js
@@ -0,0 +1,14 @@
+function bubbleSort(nums) {
+  do {
+    var swapped = false;
+    for (var i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
+        var temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return nums;
+}
