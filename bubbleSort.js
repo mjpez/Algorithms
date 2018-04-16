6703006a33d8f4abc438ae35899631491b7725fd
Mark Lopez <molopez492@gmail.com>
2018-04-16 16:10:40 -0400

    refactor: change bubbleSort logic to ES6

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,9 +1,16 @@
-function bubbleSort(nums) {
+/*
+  Bubble sort works by comparing two adjacent numbers next to each other and then
+  swapping their places if the smaller index's value is larger than the larger
+  index's. Continue looping through until all values are in ascending order
+*/
+
+const bubbleSort = (nums) => {
+  let swapped;
   do {
-    var swapped = false;
-    for (var i = 0; i < nums.length; i++) {
+    swapped = false;
+    for (let i = 0; i < nums.length; i++) {
       if (nums[i] > nums[i + 1]) {
-        var temp = nums[i];
+        const temp = nums[i];
         nums[i] = nums[i + 1];
         nums[i + 1] = temp;
         swapped = true;
